08412d6d710c10b3c96540dd2ff4f5e66e5ff4a9
andrelcorreia <andrelcorreia96@gmail.com>
2020-06-04 19:06:21 +0100

    last v

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const width = 500,
     height = 860,
-    url = encodeURI(`https://services.arcgis.com/CCZiGSEQbAxxFVh3/ArcGIS/rest/services/COVID19_ConcelhosDiarios/FeatureServer/0/query?f=json&where=ConfirmadosAcumulado>0&outFields=*&returnGeometry=false`)
-// url = encodeURI(`https://services.arcgis.com/CCZiGSEQbAxxFVh3/arcgis/rest/services/COVID19_Concelhos_V/FeatureServer/0/query?f=json&where=ConfirmadosAcumulado_Conc>0&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=ConfirmadosAcumulado_Conc desc&resultOffset=0&resultRecordCount=318&resultType=standard&cacheHint=true`)
+    url = encodeURI("https://services.arcgis.com/CCZiGSEQbAxxFVh3/arcgis/rest/services/COVID19_Concelhos_V/FeatureServer/0/query?f=json&where=ConfirmadosAcumulado_Conc>0&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=ConfirmadosAcumulado_Conc desc&resultOffset=0&resultRecordCount=318&resultType=standard&cacheHint=true"),
+    caopUrl = "data/caop.json",
+    fixedDataUrl = "data/data.json";
 
-const projection = d3.geoMercator().center([-8.00, 39.60])
+const projection = d3.geoMercator()
+    .center([-8.00, 39.60])
     .scale(7000)
     .translate([width / 2, height / 2]),
     path = d3.geoPath(projection);
@@ -16,61 +18,62 @@ const tooltip = d3.select("body").append("div")
     .attr("class", "tooltip")
     .style("opacity", 0);
 
-d3.json("caop.json")
-    .then(topology => {
+d3.json(caopUrl)
+    .then(topology => {// Obtem o topojson no ficheiro caop.json
         d3.json(url)
-            .then(data => {
-                // console.log("data", data)
-                // console.log("topojson", topology)
-                var geojson = topojson.feature(topology, topology.objects.onlydis);
-                // console.log("geojson", geojson)
-                let maxCases = 0;
-
-                let currentDates = [];
-                for (let i = 0; data.features.length > i; i++) {
-                    if (currentDates.find(element => element === data.features[i].attributes.Data)===undefined) currentDates.push(data.features[i].attributes.Data);
-                }
-                console.log(currentDates);
-                for (let i = 0; geojson.features.length > i; i++) {
-                    for (let y = 0; data.features.length > y; y++) {
-                        if (!geojson.features[i].properties.data) geojson.features[i].properties.data = []
-                        if (data.features[y].attributes.Dicofre === geojson.features[i].properties.Dicofre.substring(0, 4)) {
-                            geojson.features[i].properties = { ...geojson.features[i].properties, data: [...geojson.features[i].properties.data, data.features[y].attributes] }
-                            if (maxCases < data.features[y].attributes.ConfirmadosAcumulado_Conc) maxCases = data.features[y].attributes.ConfirmadosAcumulado_Conc
-                        }
-                    }
-                }
-
-                console.log(geojson);
-
-                svg.selectAll("path")
-                    .data(geojson.features)
-                    .enter().append("path")
-                    .attr("d", path)
-                    .style("fill", function (d) {
-                        // console.log(d)
-                        const caseOp = d.properties.ConfirmadosAcumulado_Conc / maxCases;
-                        return `rgba(200, 0, 0, ${caseOp < .05 ? .05 : caseOp})`;
-                    })
-                    .on("mouseover", function (d) {
-                        tooltip.transition()
-                            .duration(300)
-                            .style("opacity", .9);
-                        tooltip.html(
-                            `<ul>
-                            <li>${d.properties.Concelho}, ${d.properties.Distrito}</li>
-                            <li>Confirmados: ${d.properties.ConfirmadosAcumulado_Conc || 0}</li>
-                            <li>Mortos: ${d.properties.Obitos_Conc || 0}</li>
-                            <li>Recuperados: ${d.properties.Recuperados_Conc || 0}</li>
-                        </ul>`
-                        )
-                            .style("left", (d3.event.pageX) + "px")
-                            .style("top", (d3.event.pageY - 80) + "px");
-                    })
-                    .on("mouseout", function (d) {
-                        tooltip.transition()
-                            .duration(300)
-                            .style("opacity", 0);
-                    });;
-            }).catch(console.error);
+            .then(data => processAndRender(topology, data))// Obtem os dados no url original da ESRI Portugal
+            .catch(error => { // Se não for obtidos os dados com sucesso carrega uma fonte de dados predefenidos no ficheiro data.json
+                console.error(error)
+                d3.json(fixedDataUrl)
+                    .then((data) => processAndRender(topology, data))
+                    .catch(console.error)
+            });
     }).catch(console.error);
+
+
+function processAndRender(topology, data) {
+    // Chama a função feature do modulo topojson para converter o topojson para um geojson
+    var geojson = topojson.feature(topology, topology.objects.onlydis);
+    let maxCases = 0;
+    // Loop usado para juntar os dados geograficos com os dados sobre o covid-19 através do código Dicofre
+    for (let i = 0; geojson.features.length > i; i++) {
+        for (let y = 0; data.features.length > y; y++) {
+            if (data.features[y].attributes.Dico === geojson.features[i].properties.Dicofre.substring(0, 4)) {
+                geojson.features[i].properties = { ...geojson.features[i].properties, ...data.features[y].attributes }
+                // Esta condição é usada para determinar o número maximo de casos confirmados
+                if (maxCases < data.features[y].attributes.ConfirmadosAcumulado_Conc) maxCases = data.features[y].attributes.ConfirmadosAcumulado_Conc
+            }
+        }
+    }
+
+    svg.selectAll("path")
+        .data(geojson.features)
+        .enter()
+        .append("path")
+        .attr("d", path)
+        .style("stroke", "rgba(0, 0, 0, 0.2)")
+        .style("fill", function (d) {
+            const caseOp = d.properties.ConfirmadosAcumulado_Conc / maxCases;
+            return `rgba(200, 0, 0, ${caseOp < .05 ? .05 : caseOp})`;
+        })
+        .on("mouseover", function (d) {
+            d3.select(this).style('stroke', 'black');
+            tooltip.transition()
+                .duration(300)
+                .style("opacity", .9);
+            tooltip.html(
+                `<ul>
+                    <li>${d.properties.Concelho}, ${d.properties.Distrito}</li>
+                    <li>Confirmados: ${d.properties.ConfirmadosAcumulado_Conc || 0}</li>
+                </ul>`
+            )
+                .style("left", (d3.event.pageX) + "px")
+                .style("top", (d3.event.pageY - 80) + "px");
+        })
+        .on("mouseout", function () {
+            d3.select(this).style('stroke', 'rgba(0, 0, 0, 0.2)');
+            tooltip.transition()
+                .duration(300)
+                .style("opacity", 0);
+        });
+}
